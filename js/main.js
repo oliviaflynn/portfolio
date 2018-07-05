bef3b04cfe606987087996c2ab07a0a217087174
oflynn <livjflynn@gmail.com>
2018-07-05 10:01:25 +0100

    updated

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Floating Back to Top Button with Smooth Scroll using CSS & JQuery https://codepen.io/desirecode/pen/MJPJqV
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+//end
